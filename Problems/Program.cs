bdb907ffda8b81ad76ad8c055fe3f9bfb566bcd2
Kusado <kusado@gmail.com>
2017-09-15 15:41:24 +0300

    [Problem 8] Solved

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 namespace Problems {
   class Program {
     static void Main(string[] args) {
-      var problem = new Problem7();
+      var problem = new Problem8();
       problem.GetResult();
       Console.ReadKey();
     }
---
+++ XUnitTestProject/TestProblem8.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject {
+  public class TestProblem8 {
+    [Theory]
+    [InlineData(13,23514624000)]
+    [InlineData(4,5832)]
+    public void ShouldReturn(int num, long res) {
+      var sut = new Problems.Problem8();
+      sut.ComputeResult(num);
+      Assert.Equal(res, sut.Result);
+    }
+
+  }
+}
